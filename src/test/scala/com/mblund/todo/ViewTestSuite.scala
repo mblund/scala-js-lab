37273e01ba5f7f937607211efdc7eda15a35c141
Magnus Berglund <mblund@gmail.com>
2015-02-13 21:45:12 +0100

    A new test of the application view

--- src/test/scala/com/mblund/todo/ViewTestSuite.scala
+++ src/test/scala/com/mblund/todo/ViewTestSuite.scala
@@ -1,43 +1,46 @@
 package com.mblund.todo
 
-import com.mblund.todo.domain.TodoEvent
+import com.mblund.todo.domain.{TodoListAdded, TodoEvent}
 import com.mblund.todo.technology.technology.Executor
 import com.mblund.todo.technology.{Command, Topic}
 import com.mblund.todo.views.ApplicationView
 import utest.{assert, _}
 
-//object ViewTestSuite extends TestSuite{
-//  val tests = TestSuite{
-//    "Problem with Running javascript test in Rhino"{
-//      'f {
-//
-//        try {
-//          implicit val eventsTopic: Topic[TodoEvent] = new Topic[TodoEvent]
-//
-//          implicit val executor: Executor = new {
-//            def run(command: Command): Unit = {
-//              println("hello")
-//            }
-//          }
-//          println("--c")
-//
-//          val view = new ApplicationView
-//
-////          import org.scalajs.jquery.jQuery
-////          jQuery("#click-me-button").click
-//
-//          assert(true)
-//        } catch {
-//          case e: Exception =>
-//            println(e)
-//            assert(false)
-//        }
-//      }
-//    }
-//    'test2{
-//      val x = 2
-//      val y = 2
-//      assert(x == y )
-//    }
-//  }
-//}
+object ViewTestSuite extends TestSuite{
+  val tests = TestSuite{
+    'argh{
+      'test2{
+
+        implicit val eventsTopic: Topic[TodoEvent] = new Topic[TodoEvent]
+        implicit val executor: Executor = new {
+          def run(command: Command): Unit = {
+
+          }
+        }
+
+        val view = new ApplicationView
+
+        assert( view.el.querySelector(".panel-title") == null )
+        eventsTopic.distribute(TodoListAdded(0))
+        assert( view.el.querySelector(".panel-title") != null )
+
+      }
+      'f {
+
+        try {
+          implicit val eventsTopic: Topic[TodoEvent] = new Topic[TodoEvent]
+          implicit val executor: Executor = new {
+            def run(command: Command): Unit = {}
+          }
+          val view = new ApplicationView
+          assert(true)
+        } catch {
+          case e: Exception =>
+            println(e)
+            assert(false)
+        }
+      }
+    }
+
+  }
+}
