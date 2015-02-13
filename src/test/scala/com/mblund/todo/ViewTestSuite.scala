8929a63d3066973baee0af8291e8abfbf29869bb
Magnus Berglund <mblund@gmail.com>
2015-02-13 21:51:29 +0100

    Added test:The application view render a new to-do list after an TodoListAdded event

--- src/test/scala/com/mblund/todo/ViewTestSuite.scala
+++ src/test/scala/com/mblund/todo/ViewTestSuite.scala
@@ -1,7 +1,7 @@
 package com.mblund.todo
 
 import com.mblund.todo.domain.{TodoListAdded, TodoEvent}
-import com.mblund.todo.technology.technology.Executor
+
 import com.mblund.todo.technology.{Command, Topic}
 import com.mblund.todo.views.ApplicationView
 import utest.{assert, _}
@@ -9,29 +9,22 @@ import utest.{assert, _}
 object ViewTestSuite extends TestSuite{
   val tests = TestSuite{
     'argh{
-      'test2{
+      "The application view render a new to-do list after an TodoListAdded event " - {
 
         implicit val eventsTopic: Topic[TodoEvent] = new Topic[TodoEvent]
-        implicit val executor: Executor = new {
-          def run(command: Command): Unit = {
-
-          }
-        }
+        implicit val executor = new { def run(command: Command): Unit = {} }
 
         val view = new ApplicationView
-
         assert( view.el.querySelector(".panel-title") == null )
         eventsTopic.distribute(TodoListAdded(0))
         assert( view.el.querySelector(".panel-title") != null )
 
       }
-      'f {
-
+      "Failing test in Rhino, 64k function size limit" - {
         try {
-          implicit val eventsTopic: Topic[TodoEvent] = new Topic[TodoEvent]
-          implicit val executor: Executor = new {
-            def run(command: Command): Unit = {}
-          }
+          implicit val eventsTopic = new Topic[TodoEvent]
+          implicit val executor = new { def run(command: Command): Unit = {} }
+
           val view = new ApplicationView
           assert(true)
         } catch {
